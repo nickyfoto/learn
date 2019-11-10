38edb4a77be20896c82401e436f4841283a6ece7
Nicky Huang <nickyfoto@gmail.com>
2019-11-10 17:18:57 -0500

    testing class

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,65 @@
 from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
 from scipy.sparse import issparse
 
+class TestSK:
+    """
+    Compare the performance of our implementation
+    with sklearn
+    """
+    def __init__(self, learn, sk, data):
+        """
+        Args:
+            attributes: list of attributes we need to compare
+            data: X_train, X_test, y_train, y_test returned from train_test_split
+        """
+        
+        self.learn = learn
+        self.sk = sk
+        self.X_train, self.X_test, self.y_train, self.y_test = data
+
+        self.learn_clf = self.learn.fit(self.X_train, self.y_train)
+        self.sk_clf = self.sk.fit(self.X_train, self.y_train)
+        # print('finished fitting')
+
+
+    def _print_results(self, clf_names, metric, description, preds):
+        for i, name in enumerate(clf_names):
+            print(f"{name} {description}", metric(self.y_train, preds[i]))
+            print()
+
+    def compare_performance(self):
+        clf_names = ['mylearn', 'sklearn']
+        
+        for name, clf in zip(clf_names, [self.learn, self.sk]):
+            print(name, clf)
+        
+        preds = [clf.predict(self.X_train) for clf in [self.learn_clf, self.sk_clf]]
+        
+        metrics = [confusion_matrix, accuracy_score]
+        descriptions = ["Confusion Matrix: \n", "Training Accuracy:"]
+
+        for i, metric in enumerate(metrics):
+            self._print_results(clf_names, metric, descriptions[i], preds)
+
+
+    def compare_attributes(self, attributes):
+        
+        for attribute in attributes:
+            print('comparing', attribute)
+            print('mylearn:', getattr(self.learn_clf, attribute))
+            print('sklearn:', getattr(self.sk_clf, attribute))
+            print()
+
 def test(classifier, X_train, X_test, y_train, y_test,
          report=False):
     """
-    evaluate the performance of a classifier
+    Evaluate the performance of One single classifier
     print confusion matrix and accuracy
+    Args:
+        report: T/F, whether to print classification report
+
+    Returns:
+        classifier: fitted classifier
     """
     print(f'testing {classifier}')
     if issparse(X_train):
--- lr.py
+++ lr.py
@@ -30,9 +30,9 @@ class LogisticRegression(BaseEstimator):
         intercept = np.ones((X.shape[0], 1))
         return np.concatenate((intercept, X), axis=1)
 
-    def log_likelihood(self, preds, target, m):
+    def log_likelihood(self, preds, target):
         ll = - (target * np.log(preds + np.finfo(float).eps) + 
-                (1 - target) * np.log(1 - preds + np.finfo(float).eps)).sum() / m
+                (1 - target) * np.log(1 - preds + np.finfo(float).eps)).sum() / self.m
         return ll
 
 
@@ -61,6 +61,7 @@ class LogisticRegression(BaseEstimator):
                                                 target = target, m=self.m)
                 if self.print_cost: print(step, cost)
                 self.costs.append(cost)
+        return self
     
     @property
     def intercept_(self):
@@ -96,36 +97,28 @@ class LogisticRegressionSGD(BaseEstimator):
                         print_cost = True,
                         C = 0,
                         penalty=None):
-        """
-        Initialization of model parameters
-        """
+        self.num_iterations = num_iterations
         self.learning_rate = learning_rate
         self.fit_intercept = fit_intercept
-        self.num_iterations = num_iterations
         self.print_cost = print_cost
-        if C is 0: 
-            self.C = 0
-        else:
-            self.C = 1 / C
+        self.C = 1 / C if C != 0 else 0
         self.penalty = penalty
 
     def _fit_intercept(self, X):
         intercept = np.ones((X.shape[0], 1))
         return np.concatenate((intercept, X), axis=1)
     
-    def log_likelihood(self, preds, target, m):
-        ll = - (target * np.log(preds) + (1 - target) * np.log(1 - preds)).sum() / m
+    def log_likelihood(self, preds, target):
+        ll = - (target * np.log(preds + np.finfo(float).eps) + 
+                (1 - target) * np.log(1 - preds + np.finfo(float).eps)).sum() / self.m
         return ll
 
     def fit(self, features, target):
-        """
-        Update model using a pair of training sample
-        """
+
         if self.fit_intercept:
-            # intercept = np.ones((features.shape[0], 1))
             features = self._fit_intercept(features)
-        m, n = features.shape
-        self.weights = np.zeros(n)
+        self.m, self.n = features.shape
+        self.weights = np.zeros(self.n)
 
         for step in range(self.num_iterations):
             for i, x in enumerate(features):
@@ -133,25 +126,30 @@ class LogisticRegressionSGD(BaseEstimator):
                 pred = sigmoid(score)
                 error = pred - target[i]
                 gradient = np.dot(error, x)
-                # self.weights[0] -= self.weights[1:] - self.C * self.weights[1:]
                 if self.penalty:
                     self.weights[0] -= self.learning_rate * gradient[0]
-                    self.weights[1:] -= self.learning_rate * (gradient[1:] + self.C * self.weights[1:] / m)
+                    self.weights[1:] -= self.learning_rate * (gradient[1:] + self.C * self.weights[1:] / self.m)
                 else:
                     self.weights -= self.learning_rate * gradient
             # Print log-likelihood every so often
             if self.print_cost and step % (self.num_iterations//10) == 0:
                 print(step, self.log_likelihood(preds = sigmoid(np.dot(features, self.weights)),
                                                 target = target,
-                                                m = m))     
-        # mimic sklearn props
-        self.intercept_ = np.array([self.weights[0]])
-        self.coef_ = self.weights[1:].reshape(1, n - 1)
+                                                m = self.m))     
+        return self
+
+    @property
+    def intercept_(self):
+        return self.weights[:1]
+    
+    @property
+    def coef_(self):
+        return self.weights[1:].reshape(1, self.n - 1)
 
     def predict(self, X):
-        return self.predict_prob(X).round()
+        return self.predict_proba(X).round()
 
-    def predict_prob(self, X):     
+    def predict_proba(self, X):     
         if self.fit_intercept:
             X = self._fit_intercept(X)
     
@@ -159,18 +157,26 @@ class LogisticRegressionSGD(BaseEstimator):
 
 
 if __name__ == '__main__':
-    from evaluation import test
+    from time import time
+
+    from evaluation import test, TestSK
     from utils import load_beckernick, load_iris_2D, load_data
     from sklearn.model_selection import train_test_split
     from sklearn import linear_model
     
 
-    def sklearn_compare(own, sk, X_train, X_test, y_train, y_test):
-        own = test(own, X_train, X_test, y_train, y_test)
-        sk = test(sk, X_train, X_test, y_train, y_test)
-        print('comparing weights difference:')
-        print(own.intercept_, own.coef_)
-        print(sk.intercept_, sk.coef_)
+    def sklearn_compare(learn, sk, X_train, X_test, y_train, y_test):
+        data = (X_train, X_test, y_train, y_test)
+        ts = TestSK(learn=learn,
+                    sk=sk, 
+                    data=data)
+
+        attributes = ['intercept_', 'coef_']
+        ts.compare_performance()
+        print()
+        ts.compare_attributes(attributes)
+        print("="*80)
+
 
     def test_lr(X, y, clf1, clf2):
         X_train, X_test, y_train, y_test = train_test_split(X,
@@ -194,17 +200,25 @@ if __name__ == '__main__':
     sgd_r = LogisticRegressionSGD(num_iterations=500, learning_rate =1e-2,
                                 print_cost=False, penalty='l2', C=0.3)
 
-    X, y = load_beckernick()
-    test_lr(X=X, y=y, clf1=clf, clf2=skclf)
-    test_lr(X=X, y=y, clf1=sgd, clf2=skclf)
-    test_lr(X=X, y=y, clf1=clf_r, clf2=skclf_r)
-    test_lr(X=X, y=y, clf1=sgd_r, clf2=skclf_r)
 
-    X, y = load_iris_2D()
-    test_lr(X=X, y=y, clf1=clf, clf2=skclf)
-    test_lr(X=X, y=y, clf1=sgd, clf2=skclf)
-    test_lr(X=X, y=y, clf1=clf_r, clf2=skclf_r)
-    test_lr(X=X, y=y, clf1=sgd_r, clf2=skclf_r)
 
+    start = time()
+    print("Start testing")
+
+    g1 = [clf, sgd, clf_r, sgd_r]
+    g2 = [skclf, skclf, skclf_r, skclf_r]
+
+
+    datasets = [
+                load_beckernick, 
+                load_iris_2D           
+                ]
+    for dataset in datasets:
+        X, y = dataset()
+        [test_lr(X=X, y=y, clf1=c1, clf2=c2) for c1, c2 in zip(g1, g2)]
+
+    print('End testing')
+    end = time()
+    print(end - start)
 
 
