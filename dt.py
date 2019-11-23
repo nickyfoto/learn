2b5da685ac28a1b12a6928c57dd9f0e31a33a684
Nicky Huang <nickyfoto@gmail.com>
2019-11-23 10:20:43 -0500

    dt

---
+++ dt.py
@@ -0,0 +1,196 @@
+import numpy as np
+from scipy.stats import pearsonr
+from collections import Counter  
+from sklearn.base import BaseEstimator
+from math import log2
+
+def get_pearsonr(feature, Y):
+    return abs(pearsonr(feature, Y)[0])
+
+def entropy(class_y):
+
+    res = 0
+    c = Counter(class_y)
+    m = len(class_y)
+    for k, v in c.items():
+        res -= (v/m) * log2(v/m)
+    return res
+
+def _information_gain(current_y, y_entropy):
+
+    current_y_length = current_y[0].shape[0] + current_y[1].shape[0]
+    # current_entropy = sum([entropy(c) * len(c) / current_y_length for c in current_y])
+    c0 = entropy(current_y[0]) * current_y[0].shape[0] / current_y_length
+    c1 = entropy(current_y[1]) * current_y[1].shape[0] / current_y_length
+    return y_entropy - c0 - c1
+
+# def partition_classes(X, y, split_attribute, split_val):
+
+#     indices = range(len(y))
+#     if type(split_val) == str:
+#         left_indices = [i for i in indices if X[i][split_attribute] == split_val]
+#     else:
+#         left_indices = [i for i in indices if X[i][split_attribute] <= split_val]
+#     X_left = [X[i] for i in left_indices]
+#     y_left = [y[i] for i in left_indices]
+#     X_right = [X[i] for i in indices if i not in left_indices]
+#     y_right = [y[i] for i in indices if i not in left_indices]
+#     return X_left, X_right, y_left, y_right
+
+def _partition_classes(X, y, split_attribute, split_val):
+
+    left_indices = X[:,split_attribute] <= split_val
+    X_left = X[left_indices]
+    y_left = y[left_indices]
+    X_right = X[~left_indices]
+    y_right = y[~left_indices]
+    return X_left, X_right, y_left, y_right
+
+def _find_best_split(X, y, split_attribute, y_entropy):
+    
+    vals = [x[split_attribute] for x in X]
+    unique_vals = np.unique(vals)
+    info_gains = []
+    for i in range(len(unique_vals)):
+        _, _, y_left, y_right = _partition_classes(X, y, split_attribute, unique_vals[i])
+        current_y = [y_left, y_right]
+        info_gains.append(_information_gain(current_y, y_entropy))
+    # print(info_gains)
+    max_idx = np.argmax(info_gains)
+    return unique_vals[max_idx], info_gains[max_idx]
+
+
+def _find_best_feature(X, y):
+    
+    info_gains = []
+    split_vals = []
+    split_attributes = range(X[0].shape[0])
+    y_entropy = entropy(y)
+    for split_attribute in split_attributes:
+        split_val, info_gain = _find_best_split(X, y, split_attribute, y_entropy=y_entropy)
+        info_gains.append(info_gain)
+        split_vals.append(split_val)
+    max_idx = np.argmax(info_gains)
+    return split_attributes[max_idx], split_vals[max_idx]
+
+
+class DecisionTree(BaseEstimator):
+                                                                
+    def __init__(self, 
+                    verbose = False,
+                    criterion = 'pearsonr',
+                    leaf_size = 1
+                    ):   
+        self.leaf_size = 1
+        self.tree = None
+        self.criterion = criterion
+
+
+    def _get_criterion_score(self, dataX, dataY, split_func):
+        n_features= dataX.shape[1]
+        # if feature i have the same value in all dataX
+        # return (0, i)
+        return sorted([(0, i) if (dataX[:,i][0] == dataX[:,i]).all() else 
+                        # (abs(pearsonr(dataX[:, i], dataY)[0]), i) 
+                        (split_func(dataX[:, i], dataY), i) 
+                        for i in range(n_features)])
+
+    def _cannot_split(self, dataY):
+        n_train = np.array(dataY).shape[0]
+        return (n_train <= self.leaf_size or len(np.unique(dataY)) == 1)
+
+    def splited(self, left_index):
+        """
+        successfully splited
+        """
+        return len(np.unique(left_index)) > 1
+        
+    def _build_tree(self, dataX, dataY):
+        """Builds the Decision Tree recursively by choosing the best feature to split on and 
+        the splitting value. The best feature has the highest absolute correlation with dataY. 
+        If all features have the same absolute correlation, choose the first feature. The 
+        splitting value is the median of the data according to the best feature. 
+        If the best feature doesn't split the data into two groups, choose the second best 
+        one and so on; if none of the features does, return leaf
+        Parameters:
+        dataX: A numpy ndarray of X values at each node
+        dataY: A numpy 1D array of Y values at each node
+        
+        Returns:
+        tree: A numpy ndarray. Each row represents a node and four columns are feature indices 
+        (int type; index for a leaf is -1), splitting values, and starting rows, from the current 
+        root, for its left and right subtrees (if any)
+        """
+        print(np.array(dataX).shape, np.array(dataY).shape)
+        # Leaf value is the most common dataY
+        leaf_node = np.array([[-1, Counter(dataY).most_common(1)[0][0], np.nan, np.nan]])
+        
+        if self._cannot_split(dataY):
+            return leaf_node
+
+        if self.criterion == 'pearsonr':
+            feature_corrs = self._get_criterion_score(dataX, dataY, get_pearsonr)
+            while feature_corrs:
+                feature_to_split = feature_corrs.pop()[1]
+                split_val = np.median(dataX[:, feature_to_split])
+                left_index = dataX[:, feature_to_split] <= split_val
+                right_index = dataX[:, feature_to_split] > split_val
+                if self.splited(left_index):
+                    break
+            if not feature_corrs:
+                return leaf_node
+            
+            left_tree = self._build_tree(dataX[left_index], dataY[left_index])
+            right_tree = self._build_tree(dataX[right_index], dataY[right_index])    
+            root = np.array([[feature_to_split, split_val, 1, left_tree.shape[0] + 1]])
+            res = np.vstack((root, left_tree, right_tree))
+            return res
+        else:
+            feature_to_split, split_val = _find_best_feature(dataX, dataY)
+            X_left, X_right, y_left, y_right = _partition_classes(dataX, dataY, 
+                                                feature_to_split, split_val)
+            left_tree = self._build_tree(X_left, y_left)
+            right_tree = self._build_tree(X_right, y_right)    
+            root = np.array([[feature_to_split, split_val, 1, left_tree.shape[0] + 1]])
+            res = np.vstack((root, left_tree, right_tree))
+            return res
+
+
+    def _search_point(self, point, row):
+        """A private function to be used with query. It recursively searches 
+        the decision tree matrix and returns a predicted value for point.
+        Parameters:
+        point: A numpy 1D array of test query
+        row: The row of the decision tree matrix to search
+    
+        Returns 
+        pred: The predicted value
+        """
+        loc, split_val = self.tree[row, :2]
+        if loc == -1:
+            return split_val
+        elif point[int(loc)] <= split_val:
+            pred = self._search_point(point, row + int(self.tree[row, 2]))
+        else:
+            pred = self._search_point(point, row + int(self.tree[row, 3]))
+        return pred
+
+    def fit(self, dataX, dataY):
+        # print(dataY.shape)
+        new_tree = self._build_tree(dataX, dataY)
+        if not self.tree:
+            self.tree = new_tree
+        else:
+            self.tree = np.vstack((self.tree, new_tree))
+        return self
+
+    def predict(self, points):
+        """Estimates a set of test points given the model we built
+        
+        Parameters:
+        points: A numpy ndarray of test queries
+        Returns: 
+        preds: A numpy 1D array of the estimated values
+        """                                                         
+        preds = [self._search_point(point, row=0) for point in points]
+        return preds
---
+++ test_dt.py
@@ -0,0 +1,133 @@
+"""
+pytest -svv test_dt.py
+pytest -svv -m 'not smoke' test_dt.py
+pytest -vv --html=report.html --capture=sys
+
+"""
+
+import pytest
+import numpy as np
+import pandas as pd
+
+from dt import DecisionTree
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+# @pytest.mark.smoke
+def test_basic_dt():
+    X = np.array([[0, 0], 
+                  [1, 1]])
+    Y = np.array([0, 1])
+    sk_clf = tree.DecisionTreeClassifier(criterion='entropy', random_state=0)
+    sk_clf.fit(X, Y)
+    print()
+    # print(clf.predict([[2., 2.]]))
+    print(sk_clf)
+    print(sk_clf.predict(X))
+
+
+    clf = DecisionTree(criterion='entropy')
+    clf.fit(X, Y)
+    print(clf)
+    print(clf.predict(X))
+    print(clf.tree)
+
+
+# @pytest.mark.smoke
+def test_iris_dt():
+    from sklearn.datasets import load_iris
+    iris = load_iris()
+
+
+    X_train, X_test, y_train, y_test = train_test_split(iris.data,
+                                                        iris.target, 
+                                                        test_size = 0.25, 
+                                                        random_state = 0,
+                                                        stratify = iris.target)
+
+
+    sk_clf = tree.DecisionTreeClassifier(criterion='entropy', random_state=0)
+    sk_clf.fit(X_train, y_train)
+    print()
+    # print(clf.predict([[2., 2.]]))
+    print(sk_clf)
+
+    sk_acc = accuracy_score(y_true=y_train, y_pred=sk_clf.predict(X_train))
+    sk_test_acc = accuracy_score(y_true=y_test, y_pred=sk_clf.predict(X_test))
+
+    clf = DecisionTree(criterion='entropy')
+    clf.fit(X_train, y_train)
+    print()
+    # print(clf.predict([[2., 2.]]))
+    print(clf)
+
+    my_acc = accuracy_score(y_true=y_train, y_pred=clf.predict(X_train))
+    my_test_acc = accuracy_score(y_true=y_test, y_pred=clf.predict(X_test))
+    print('sk_test_acc', sk_test_acc)
+    print('my_test_acc', my_test_acc)
+    assert sk_acc == my_acc
+
+
+
+
+
+def test_hw4_dt():
+    data_test = pd.read_csv("datasets/hw4_data_test.csv")
+    data_valid = pd.read_csv("datasets/hw4_data_valid.csv")
+    data_train = pd.read_csv("datasets/hw4_data_train.csv")
+
+    categorical = ['workclass', 'education', 'marital-status', 'occupation', 
+                       'relationship', 'race', 'sex', 'native-country']
+    numerical = ['age', 'fnlwgt', 'education-num','capital-gain', 'capital-loss',
+                    'hours-per-week']
+
+    for feature in categorical:
+            le = LabelEncoder()
+            data_train[feature] = le.fit_transform(data_train[feature])
+            data_test[feature] = le.fit_transform(data_test[feature])
+            
+    X_train = pd.concat([data_train[categorical], data_train[numerical]], axis=1)
+    y_train = data_train['high-income']
+    X_test = pd.concat([data_test[categorical], data_test[numerical]], axis=1)
+    y_test = data_test['high-income']
+    X_train, y_train, X_test, y_test = np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)
+
+    for feature in categorical:
+            le = LabelEncoder()
+            data_valid[feature] = le.fit_transform(data_valid[feature])  
+            
+    X_valid = pd.concat([data_valid[categorical], data_valid[numerical]], axis=1)
+    y_valid = data_valid['high-income']
+    X_valid, y_valid = np.array(X_valid), np.array(y_valid)
+
+
+
+    sk_clf = tree.DecisionTreeClassifier(criterion='entropy', random_state=0)
+    sk_clf.fit(X_train, y_train)
+    print()
+    # print(clf.predict([[2., 2.]]))
+    print(sk_clf)
+
+    sk_acc = accuracy_score(y_true=y_train, y_pred=sk_clf.predict(X_train))
+    sk_test_acc = accuracy_score(y_true=y_test, y_pred=sk_clf.predict(X_test))
+    print(sk_acc, sk_test_acc)
+
+    clf = DecisionTree(criterion='entropy')
+    # clf = DecisionTree()
+    clf.fit(X_train, y_train)
+    
+    print(clf)
+
+    my_acc = accuracy_score(y_true=y_train, y_pred=clf.predict(X_train))
+    my_test_acc = accuracy_score(y_true=y_test, y_pred=clf.predict(X_test))
+    print(my_acc, my_test_acc)
+    # assert sk_acc == my_acc
+
+# test_iris_dt()
+
+
+
+
+
